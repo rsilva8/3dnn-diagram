9cb2ead13e14bec11e8871db6ca9c3df830d83df
Rogers F Silva <rogers.silva@datalyticsolutions.com>
2017-12-19 19:01:04 -0700

    Included function to draw the weights.

--- layer.m
+++ layer.m
@@ -2,19 +2,19 @@ function [ h ] = layer( source_num, layer_num, num_cells, varargin )
 %INPUT_LAYER Draw an input layer in 3D.
 %   source_num: data source number
 %   layer_num: layer number
-%   num_cells: numer of cells in layer
+%   num_cells: number of cells in layer
 %   varargin: specify additional patch properties
 %   
 %   h: patch objects
 
-diam = .7;
-off = diam/2;
+diam = .5;
+off = diam;
 width = diam/sqrt(2);
 center = [3*(source_num-1),0,3*(layer_num-1)];
-h = cell(1,num_cells);
+h = cell(1,num_cells+1);
 
 % Draw layer rectangle
-side1 = 1.5*diam;
+side1 = 1.75*diam;
 side2 = (num_cells-1)*(diam+off);
 center2 = side2/2;
 side2 = side2 + (diam+off);
@@ -22,7 +22,7 @@ h{1} = rect(center+[0,center2,0],side1,side2,varargin{:});
 hold on
 % Draw input cells
 for cc = 1:num_cells
-    new_center = center + [0,(cc-1)*(diam+off),0];
+    new_center = center + [0,(cc-1)*(diam+off),.01*center(3)];
     if layer_num == 1
         h{cc+1} = square(new_center,width,varargin{:},'FaceColor','black');
     else
---
+++ weights.m
@@ -0,0 +1,45 @@
+function [ h ] = weights( connection, source_nums, layer_nums, nums_cells, varargin )
+%INPUT_LAYER Draw an input layer in 3D.
+%   connection: 'fully_connected' or cell array {in} = [out1, out2, ...]
+%   source_nums: data source numbers [in, out]
+%   layer_nums: layer numbers [in out]
+%   nums_cells: number of cells in layers [in, out]
+%   varargin: specify additional line properties
+%   
+%   h: line objects
+
+diam = .5;
+off = diam;
+
+centers = {[3*(source_nums(1)-1),0,3*(layer_nums(1)-1)],
+           [3*(source_nums(2)-1),0,3*(layer_nums(2)-1)]};
+h = cell(1,prod(nums_cells));
+
+% Draw weights
+if iscell(connection)
+    for cc_in = 1:length(connection)
+        cc_out = connection{cc_in};
+        new_center_in = centers{1} + [0,(cc_in-1)*(diam+off),0];
+        new_center_out = centers{2} + [0,(cc_out-1)*(diam+off),0];
+        new_centers = [new_center_in;new_center_out];
+        
+        h = plot3(new_centers(:,1),new_centers(:,2),new_centers(:,3),'black',varargin{:});
+        hold on
+    end
+elseif strcmpi(connection,'fully_connected')
+    for cc_in = 1:nums_cells(1)
+        for cc_out = 1:nums_cells(2)
+            new_center_in = centers{1} + [0,(cc_in-1)*(diam+off),0];
+            new_center_out = centers{2} + [0,(cc_out-1)*(diam+off),0];
+            new_centers = [new_center_in;new_center_out];
+
+            h = plot3(new_centers(:,1),new_centers(:,2),new_centers(:,3),'black',varargin{:});
+            hold on
+        end
+    end
+else
+    error('Invalid connection parameter')
+end
+
+end
+
